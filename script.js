e96475c67d3493c6e6a551e31ab4beabce372bcd
Elina <Elel0001@stud.kea.dk>
2024-02-06 15:34:28 +0100

    udkommenteret

--- script.js
+++ script.js
@@ -54,7 +54,10 @@ function showClothes(clothesJSON) {
     clothesClone.querySelector(".clothes_productdisplayname").textContent = clothes.productdisplayname;
     clothesClone.querySelector(".clothes_brandname").textContent = clothes.brandname;
     clothesClone.querySelector(".clothes_price").textContent = clothes.price;
+
     clothesClone.querySelector(".clothes_discount").textContent = clothes.discount;
+    // if (clothes.discount) {
+    // }
 
     if (clothes.soldout) {
       clothesClone.querySelector(".clothes_image").classList.add("image_soldout");
