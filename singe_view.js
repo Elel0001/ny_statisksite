464f47fa23932a2ac5a36c5e40ba3bd4416d9f95
Elina <Elel0001@stud.kea.dk>
2024-02-07 10:22:57 +0100

    styling af singleview soldout - virker

--- singe_view.js
+++ singe_view.js
@@ -8,17 +8,15 @@ fetch(url)
   .then((data) => showClothes(data));
 
 function showClothes(product) {
-  const clothesClone = clothesTemplate.cloneNode(true).content;
+  console.log(product);
 
-  // console.log(product);
   document.querySelector(".purchaseBox h3").textcontent = product.productdisplayname;
   document.querySelector(".purchaseBox .brand").textcontent = product.brandname;
   document.querySelector(".images_singleview").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 
   if (product.soldout) {
-    console.log("hej");
-    clothesClone.querySelector(".image_singleview").classList.add("image_soldout");
-    clothesClone.querySelector(".clothes_box").classList.add("clothes_soldout");
-    clothesClone.querySelector(".clothes_box").textContent = "Sold Out";
+    document.querySelector(".images_singleview").classList.add("image_soldout");
+    document.querySelector(".clothes_box").classList.add("clothes_soldout_single");
+    document.querySelector(".clothes_box").textContent = "Sold Out";
   }
 }
